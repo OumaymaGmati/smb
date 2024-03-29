6bc1c5f780aac40028438edd60004439052a568c
OumaymaBenMiled <oumaymabenmiled59@gmail.com>
2024-03-29 09:41:25 +0100

    smb

--- js/transalte.js
+++ js/transalte.js
@@ -21,8 +21,8 @@ const translations={
         start:"بدء المحادثة",
         visit:"زيارة مكتبنا",
         find:"العثور علينا",
-        txt:"  SMB هي وكالة دولية متخصصة في الرياضة على مستوى عالمي، مقرها " ,
-        dubai:" في دبي",
+        txt:"  SMB هي وكالة دولية متخصصة في الرياضة على مستوى عالمي،  " ,
+        dubai:"مقرها في دبي",
         Launching :"الإطلاق في " ,
         days:"أيام",
         h:"س",
@@ -94,102 +94,116 @@ const translations={
 
 }
 
-$(document).ready(function() {
-    $('#selectpicker').on('change', function() {
+$(document).ready(function () {
+    $('#selectpicker').on('change', function () {
         const lang = $(this).val();
-            if(lang=="ar"){  
-                $('#welcome').text(translations.ar.welcome);
-        $('#welcomtosbm').html(translations.ar.welcomtosbm);
-        $('#title2').text(translations.ar.title2);
-        $('#start').text(translations.ar.start);
-        $('#visit').text(translations.ar.visit);
-        $('#find').text(translations.ar.find);
-        $('#txt').text(translations.ar.txt);
-        $('#Launching').text(translations.ar.Launching);
-        $('#days').text(translations.ar.days);
-        $('#h').text(translations.ar.h);
-        $('#m').text(translations.ar.m);
-        $('#s').text(translations.ar.s);
-        $('#dubai').text(translations.ar.dubai);
-            
-              
-            }else if(lang=="en"){  
-                $('#welcome').text(translations.en.welcome);
-                $('#welcomtosbm').html(translations.en.welcomtosbm);
-                $('#title2').text(translations.en.title2);
-                $('#start').text(translations.en.start);
-                $('#visit').text(translations.en.visit);
-                $('#find').text(translations.en.find);
-                $('#txt').text(translations.en.txt);
-                $('#Launching').text(translations.en.Launching);
-                $('#days').text(translations.en.days);
-                $('#h').text(translations.en.h);
-                $('#m').text(translations.en.m);
-                $('#s').text(translations.en.s);
-                $('#dubai').text(translations.en.dubai);
-              
-            }else if(lang=="fr"){
-                $('#welcome').text(translations.fr.welcome);
-        $('#welcomtosbm').html(translations.fr.welcomtosbm);
-        $('#title2').text(translations.fr.title2);
-        $('#start').text(translations.fr.start);
-        $('#visit').text(translations.fr.visit);
-        $('#find').text(translations.fr.find);
-        $('#txt').text(translations.fr.txt);
-        $('#Launching').text(translations.fr.Launching);
-        $('#days').text(translations.fr.days);
-        $('#h').text(translations.fr.h);
-        $('#m').text(translations.fr.m);
-        $('#s').text(translations.fr.s);
-        $('#dubai').text(translations.fr.dubai);
-            }else if(lang=="de"){
-                $('#welcome').text(translations.de.welcome);
-                $('#welcomtosbm').html(translations.de.welcomtosbm);
-                $('#title2').text(translations.de.title2);
-                $('#start').text(translations.de.start);
-                $('#visit').text(translations.de.visit);
-                $('#find').text(translations.de.find);
-                $('#txt').text(translations.de.txt);
-                $('#Launching').text(translations.de.Launching);
-                $('#days').text(translations.de.days);
-                $('#h').text(translations.de.h);
-                $('#m').text(translations.de.m);
-                $('#s').text(translations.de.s);
-                $('#dubai').text(translations.de.dubai);
-            }
-            else if(lang=="es"){
-                $('#welcome').text(translations.es.welcome);
-                $('#welcomtosbm').html(translations.es.welcomtosbm);
-                $('#title2').text(translations.es.title2);
-                $('#start').text(translations.es.start);
-                $('#visit').text(translations.es.visit);
-                $('#find').text(translations.es.find);
-                $('#txt').text(translations.es.txt);
-                $('#Launching').text(translations.es.Launching);
-                $('#days').text(translations.es.days);
-                $('#h').text(translations.es.h);
-                $('#m').text(translations.es.m);
-                $('#s').text(translations.es.s);
-                $('#dubai').text(translations.es.dubai);
-            }   else if(lang=="ru"){
-                $('#welcome').text(translations.ru.welcome);
-                $('#welcomtosbm').html(translations.ru.welcomtosbm);
-                $('#title2').text(translations.ru.title2);
-                $('#start').text(translations.ru.start);
-                $('#visit').text(translations.ru.visit);
-                $('#find').text(translations.ru.find);
-                $('#txt').text(translations.ru.txt);
-                $('#Launching').text(translations.ru.Launching);
-                $('#days').text(translations.ru.days);
-                $('#h').text(translations.ru.h);
-                $('#m').text(translations.ru.m);
-                $('#s').text(translations.ru.s);
-                $('#dubai').text(translations.ru.dubai);
-            }
-        
-        
-        
-       
+        if (lang == "ar") {
+            $('#welcome').text(translations.ar.welcome);
+            $('#welcomtosbm').html(translations.ar.welcomtosbm);
+            $('#title2').text(translations.ar.title2);
+            $('#title2').css('direction', 'rtl');
+            $('#start').text(translations.ar.start);
+            $('#visit').text(translations.ar.visit);
+            $('#find').text(translations.ar.find);
+            $('#txt').text(translations.ar.txt);
+            $('.txt2').css('direction', 'rtl');
+            $('#Launching').text(translations.ar.Launching);
+            $('#days').text(translations.ar.days);
+            $('#h').text(translations.ar.h);
+            $('#m').text(translations.ar.m);
+            $('#s').text(translations.ar.s);
+            $('#dubai').text(translations.ar.dubai);
+            $('.dubaicss').css('direction', 'rtl');
+        } else if (lang == "en") {
+            $('#welcome').text(translations.en.welcome);
+            $('#welcomtosbm').html(translations.en.welcomtosbm);
+            $('#title2').text(translations.en.title2);
+            $('#title2').css('direction', '');
+            $('#start').text(translations.en.start);
+            $('#visit').text(translations.en.visit);
+            $('#find').text(translations.en.find);
+            $('#txt').text(translations.en.txt);
+            $('.txt2').css('direction', '');
+            $('#Launching').text(translations.en.Launching);
+            $('#days').text(translations.en.days);
+            $('#h').text(translations.en.h);
+            $('#m').text(translations.en.m);
+            $('#s').text(translations.en.s);   
+             $('#dubai').text(translations.en.dubai);
+            $('.dubaicss').css('direction', '');
+        } else if (lang == "fr") {
+            $('#welcome').text(translations.fr.welcome);
+            $('#welcomtosbm').html(translations.fr.welcomtosbm);
+            $('#title2').text(translations.fr.title2);
+            $('#title2').css('direction', '');
+            $('#start').text(translations.fr.start);
+            $('#visit').text(translations.fr.visit);
+            $('#find').text(translations.fr.find);
+            $('#txt').text(translations.fr.txt);
+            $('.txt2').css('direction', '');
+            $('#Launching').text(translations.fr.Launching);
+            $('#days').text(translations.fr.days);
+            $('#h').text(translations.fr.h);
+            $('#m').text(translations.fr.m);
+            $('#s').text(translations.fr.s);    
+             $('#dubai').text(translations.fr.dubai);
+               $('.dubaicss').css('direction', '');
+        } else if (lang == "de") {
+            $('#welcome').text(translations.de.welcome);
+            $('#welcomtosbm').html(translations.de.welcomtosbm);
+            $('#title2').text(translations.de.title2);
+            $('#title2').css('direction', '');
+            $('#start').text(translations.de.start);
+            $('#visit').text(translations.de.visit);
+            $('#find').text(translations.de.find);
+            $('#txt').text(translations.de.txt);
+            $('.txt2').css('direction', '');
+            $('#Launching').text(translations.de.Launching);
+            $('#days').text(translations.de.days);
+            $('#h').text(translations.de.h);
+            $('#m').text(translations.de.m);
+            $('#s').text(translations.de.s);
+            $('#dubai').text(translations.de.dubai);
+            $('.dubaicss').css('direction', '');
+        }
+        else if (lang == "es") {
+            $('#welcome').text(translations.es.welcome);
+            $('#welcomtosbm').html(translations.es.welcomtosbm);
+            $('#title2').text(translations.es.title2);
+            $('#title2').css('direction', '');
+            $('#start').text(translations.es.start);
+            $('#visit').text(translations.es.visit);
+            $('#find').text(translations.es.find);
+            $('#txt').text(translations.es.txt);
+            $('.txt2').css('direction', '');
+            $('#Launching').text(translations.es.Launching);
+            $('#days').text(translations.es.days);
+            $('#h').text(translations.es.h);
+            $('#m').text(translations.es.m);
+            $('#s').text(translations.es.s);
+            $('#dubai').text(translations.es.dubai);
+            $('.dubaicss').css('direction', '');
+        } else if (lang == "ru") {
+            $('#welcome').text(translations.ru.welcome);
+            $('#welcomtosbm').html(translations.ru.welcomtosbm);
+            $('#title2').text(translations.ru.title2);
+            $('#title2').css('direction', '');
+            $('#start').text(translations.ru.start);
+            $('#visit').text(translations.ru.visit);
+            $('#find').text(translations.ru.find);
+            $('#txt').text(translations.ru.txt);
+            $('.txt2').css('direction', '');
+            $('#Launching').text(translations.ru.Launching);
+            $('#days').text(translations.ru.days);
+            $('#h').text(translations.ru.h);
+            $('#m').text(translations.ru.m);
+            $('#s').text(translations.ru.s);
+            $('#dubai').text(translations.ru.dubai);
+            $('.dubaicss').css('direction', '');
+        }
+
+
+
     });
 });
 
